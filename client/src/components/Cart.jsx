82538c33c2c11454f7c12bf7651658f94c6c6cff
Jiaying330 <jiaying.hou330@gmail.com>
2024-06-07 10:20:04 -0400

    add responsiveness

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,64 +1,68 @@
-import React, { useEffect, useState } from 'react';
-import './Cart.css';
-import ClearIcon from '@mui/icons-material/Clear';
+import React, { useEffect, useState } from "react";
+import "./Cart.css";
+import ClearIcon from "@mui/icons-material/Clear";
 import CartItem from "./CartItem";
 import { useCart } from "../context/CartContext";
-import { Link } from 'react-router-dom';
-import DepartmentItem from '../components/DepartmentItem';
-import useFetchArtworkDetails from '../hooks/useFetchArtworkDetails';
+import { Link } from "react-router-dom";
+import DepartmentItem from "../components/DepartmentItem";
+import useFetchArtworkDetails from "../hooks/useFetchArtworkDetails";
 
 export default function Cart(props) {
-    const { cartItems, closeCart, isOpen } = useCart();
+  const { cartItems, closeCart, isOpen } = useCart();
 
-    const { artworksByDepartment, loading, error } = useFetchArtworkDetails(cartItems);
+  const { artworksByDepartment, loading, error } =
+    useFetchArtworkDetails(cartItems);
 
-    if (loading) {
-        return (
-            <div className="cart-details-page-container">
-                <h1>Cart Detail</h1>
-                <div>Loading...</div>
-            </div>
-        )
-    }
-
-    if (error) {
-        return  (
-        <div className="cart-details-page-container">
-            <h1>Cart Detail</h1>
-            <div>{error}</div>
-        </div>
-        )
-    }
-
+  if (loading) {
     return (
-        // <div id="myCart" className="cart-page">
-        <div id="myCart" className="cart-container">
-            <div className="cart-items-container">
-                {artworksByDepartment.map(([department, artworks]) => (
-                    <DepartmentItem
-                        key={department}
-                        department={department}
-                        artworks={artworks}
-                    />
-                ))}
-                {/* {cartItems.map(item => (
+      <div className="cart-details-page-container">
+        <h1>Cart Detail</h1>
+        <div>Loading...</div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="cart-details-page-container">
+        <h1>Cart Detail</h1>
+        <div>{error}</div>
+      </div>
+    );
+  }
+
+  return (
+    // <div id="myCart" className="cart-page">
+    <div id="myCart" className="cart-container">
+      <div className="cart-items-container">
+        {artworksByDepartment.map(([department, artworks]) => (
+          <DepartmentItem
+            key={department}
+            department={department}
+            artworks={artworks}
+          />
+        ))}
+        {/* {cartItems.map(item => (
                         <CartItem key={item} objectID={item} />
                     ))} */}
-            </div>
-            {/* } */}
-            <div className="cart-header">
-                Your Cart
-                <a onClick={closeCart}><ClearIcon /></a>
-            </div>
-            <div className="cart-footer">
-                <Link to="/cart-detail">View Detail</Link>
-                {/* <a onClick={() => {
+      </div>
+      {/* } */}
+      <div className="cart-header">
+        Your Cart
+        <a onClick={closeCart}>
+          <ClearIcon />
+        </a>
+      </div>
+      <div className="cart-footer">
+        <Link to="/cart-detail" onClick={closeCart}>
+          View Detail
+        </Link>
+        {/* <a onClick={() => {
 
                     }}>View Detail</a> */}
-            </div>
-            {/* </div> */}
-        </div>
-        // </div>
-
-    )
+      </div>
+      {/* </div> */}
+    </div>
+    // </div>
+  );
 }
